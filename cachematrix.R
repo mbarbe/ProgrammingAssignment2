b1241893e43248bd530ff44a51a152bb6d5b6954
mbarbe <mbarbe@gmail.com>
2015-02-10 16:16:11 -0300

    add and modify comments

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,7 @@ makeCacheMatrix <- function(x = matrix()) {
         # Clean m that act as cache
         m <- NULL 
         # Clean the cache and assign to x the value of y 
-        # to pass values when makeCacheMatrix is called without arguments
+        # to modify the initial value of the matrix
         set <- function(y) { 
                 x <<- y      
                 m <<- NULL
@@ -16,8 +16,7 @@ makeCacheMatrix <- function(x = matrix()) {
         setinverse <- function(inverse) m <<- inverse
         # Retrieve the inverse matrix from cache
         getinverse <- function() m 
-        # Return a list of the functions x$set(), x$get(),  
-        # x$setinverse() and x$getinverse
+        # Return a list of the functions
         list(set = set, get = get,    
              setinverse = setinverse, 
              getinverse = getinverse)
@@ -28,17 +27,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Return a matrix that is the inverse of 'x' using the functions provided
 ## by makeCacheMatrix or calculating the inverse matrix if it not in the cache
 ## and saving it in the cache.
+
 cacheSolve <- function(x, ...) {
-        # Retrieve from cache inverse matrix assoc. w/x
+        # Retrieve from cache inverse matrix associated with x
         m <- x$getinverse() 
         # Check cache and if it is valid, notify and return it to the user
+        # Ending the function
         if(!is.null(m)) {
                 message("getting cached data") 
                 return(m)
         }
         # If cache not valid, obtain the matrix
         matrix <- x$get() 
-        # Calculate the inverse of the matrix
+        # Calculate the inverse of the matrix obtained
         m <- solve(matrix, ...)
         # Save the inverse matrix into the cache
         x$setinverse(m)
